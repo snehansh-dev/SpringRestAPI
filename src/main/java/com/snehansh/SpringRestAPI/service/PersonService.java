a955653c9787149746e2c0196133260cfdef6ad8
Shekhar <dev.snehansh@gmail.com>
2023-01-18 01:37:07 +0530

    CRUD with Rest API is done

--- src/main/java/com/snehansh/SpringRestAPI/service/PersonService.java
+++ src/main/java/com/snehansh/SpringRestAPI/service/PersonService.java
@@ -2,6 +2,16 @@ package com.snehansh.SpringRestAPI.service;
 
 import com.snehansh.SpringRestAPI.entity.Person;
 
+import java.util.List;
+
 public interface PersonService {
     public Person saveUser(Person person);
+
+    public List<Person> getUser();
+
+    public Person getUser(Long userId);
+
+    public Person updateUser(Long userId, Person person);
+
+    public String deleteUser(Long userId);
 }
--- src/main/java/com/snehansh/SpringRestAPI/service/PersonServiceImpl.java
+++ src/main/java/com/snehansh/SpringRestAPI/service/PersonServiceImpl.java
@@ -5,6 +5,9 @@ import com.snehansh.SpringRestAPI.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Objects;
+
 @Service
 public class PersonServiceImpl implements PersonService {
     @Autowired
@@ -12,5 +15,55 @@ public class PersonServiceImpl implements PersonService {
     @Override
     public Person saveUser(Person person) {
         return userRepository.save(person);
+    }
+
+    @Override
+    public List<Person> getUser() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Person getUser(Long userId) {
+        return userRepository.findById(userId).get();
+    }
+
+    @Override
+    public Person updateUser(Long userId, Person person) {
+        Person persondb = userRepository.findById(userId).get();
+        if(Objects.nonNull(person.getUserFirstName()) &&
+        !"".equalsIgnoreCase(person.getUserFirstName())){
+            persondb.setUserFirstName(person.getUserFirstName());
+        }
+
+        if(Objects.nonNull(person.getUserLastName()) &&
+        !"".equalsIgnoreCase(person.getUserLastName())){
+            persondb.setUserLastName(person.getUserLastName());
+        }
+        if(Objects.nonNull(person.getUserAddress()) &&
+        !"".equalsIgnoreCase(person.getUserAddress())){
+            persondb.setUserAddress(person.getUserAddress());
+        }
+        if(Objects.nonNull(person.getUserEmail()) &&
+        !"".equalsIgnoreCase(person.getUserEmail())){
+            persondb.setUserEmail(person.getUserEmail());
+        }
+        if(Objects.nonNull(person.getUserContact()) &&
+        !"".equalsIgnoreCase(person.getUserContact())){
+            persondb.setUserContact(person.getUserContact());
+        }
+        return userRepository.save(persondb);
+    }
+
+    @Override
+    public String deleteUser(Long userId) {
+        Person person = userRepository.findById(userId).get();
+        String message;
+        if(person.getUserFirstName().isBlank()){
+            message = "Invalid User Id";
+        }else{
+            message = "Person: ".concat(person.getUserFirstName()).concat(" ").concat(person.getUserLastName()).concat(" is deleted");
+        }
+        userRepository.deleteById(userId);
+        return message;
     }
 }
